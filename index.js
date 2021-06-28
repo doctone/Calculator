3d602db2bac53e5f58ae27c968300670fa9f9227
doctone <samjojames@gmail.com>
2021-06-28 18:43:08 +0100

    completed switch statement with inputs

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const readline = require("readline-sync");
 
 console.log('Please enter the operator of your choice: ');
 const response = readline.prompt();
-switch response {
-    case '+':
-        break;
-    case '-':
-        break
-    case '/':
-        break
-    case '*':
-        break
+console.log("How many numbers would you like to " + response + "? ")
+const stringNums = readline.prompt();
+const numbers = new Array(parseInt(stringNums))
+const args = []
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(`number ${i+1}: `)
+    const arg = readline.prompt();
+    args.push(arg)
 }
+let answer = numbers[0]
+for (let i = 0; i<args.length; i++) {
+    switch (response) {
+        case '+':
+            answer += numbers[i]
+            break;
+        case '-':
+            answer -= numbers[i]
+            break;
+        case '/':
+            answer /= numbers[i]
+            break;
+        case '*':
+            answer *= numbers[i]
+            break;
+} 
+}
+console.log(`The answer is ${answer}.`);
