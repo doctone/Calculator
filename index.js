8986ad52d0626c8174efe80a6635334e9f4fa575
doctone <samjojames@gmail.com>
2021-06-28 16:54:42 +0100

    added switch statements

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const readline = require("readline-sync");
 
 console.log('Please enter the operator of your choice: ');
 const response = readline.prompt();
+switch response {
+    case '+':
+        break;
+    case '-':
+        break
+    case '/':
+        break
+    case '*':
+        break
+}
