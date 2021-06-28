93100c67d8bbb088102e5dfedc410ec243b65f60
doctone <samjojames@gmail.com>
2021-06-28 16:52:00 +0100

    changed input message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const readline = require("readline-sync");
 
-console.log('Please enter some input:');
+console.log('Please enter the operator of your choice: ');
 const response = readline.prompt();
